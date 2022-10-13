7d305ddb37437f05af55b47e01748838cf804124
Romiza Zildjian <85002028+romizazildjian@users.noreply.github.com>
2022-10-13 22:59:29 +0800

    Add files via upload

---
+++ Pertemuan_4-JS_insert_update_delete_data/main.js
@@ -0,0 +1,57 @@
+var selected_tr = null;
+var btn = document.querySelector(".button")
+btn.addEventListener("click", studentData);
+
+function studentData(){
+  var tf_data = readData()
+  if(selected_tr == null){
+    insertData(tf_data)
+    resetData()
+  }else{
+    updateData(tf_data)
+    resetData()
+    selected_tr = null
+  }
+  resetData()
+}
+function readData(){
+  var empy = {}
+  empy["id"] = document.querySelector(".student-id").value;
+  empy["name"] = document.querySelector(".student-name").value;
+  return empy
+}
+
+function insertData(x){
+  var table = document.querySelector(".list");
+  var tr = table.insertRow(table.length);
+  var td1 = tr.insertCell(0)
+  var td2 = tr.insertCell(1)
+  var td3 = tr.insertCell(2)
+  td1.innerHTML = x.id;
+  td2.innerHTML = x.name; 
+  td3.innerHTML = "<a class='edit' onClick='editData(this)'>Edit</a> || <a onClick='deleteData(this)'>Delete</a>";
+}
+
+function resetData(){
+  document.querySelector(".student-id").value = "";
+  document.querySelector(".student-name").value = "";
+}
+
+function editData(y){
+  selected_tr = document.querySelector("a.edit");
+  selected_tr = y.parentElement.parentElement;
+  document.querySelector(".student-id").value = selected_tr.cells[0].innerHTML;
+  document.querySelector(".student-name").value = selected_tr.cells[1].innerHTML;
+}
+
+function deleteData(del) {
+  if (confirm('Are you sure to delete this record ?')) {
+  var delete_tr = del.parentElement.parentElement;
+  delete_tr.remove();
+  }
+}
+
+function updateData(){
+  selected_tr.cells[0].innerHTML = document.querySelector(".student-id").value;
+  selected_tr.cells[1].innerHTML = document.querySelector(".student-name").value;
+}
